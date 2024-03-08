b123b7b9576d29bfcdd2265467d327c88570fcaa
Matheus Bueno <mathbueeno@gmail.com>
2024-03-08 15:50:18 -0300

    Inicio

---
+++ src/routes/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Search } from "react-router-dom"
+import Seach from '../components/Search'
+
+const Home = () => {
+    return (
+        <div>
+            <Seach />
+        </div>
+    )
+}
+
+export default Home
